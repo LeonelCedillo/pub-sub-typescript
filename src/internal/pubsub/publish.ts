6ea3a1a34128074e23b75abf64122e449d78bbb4
LeonelCedillo <leonel.cs.979@gmail.com>
2026-04-15 17:49:32 -0700

    data serialization: MessagePack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import { type ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 
 // (server) Publish a message to the exchange 
@@ -17,6 +18,35 @@ export function publishJSON<T>(
             routingKey, 
             content, 
             { contentType: "application/json" },
+            (err) => {
+                if (err !== null) {
+                    reject(new Error("Message was NACKed by the broker"));
+                } else {
+                    resolve(); // Message was ACked (confirmed/accepted) by the broker
+                }
+            }
+        )
+    });
+}
+
+// MessagePack is a binary data serialization format,
+// an alternative to JSON that’s designed to be smaller and faster.
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+    // serialize value with MessagePack
+    const body: Uint8Array = encode(value);
+    const content: Buffer = Buffer.from(body);
+    // publish a message to the exchange
+    return new Promise((resolve, reject) => {
+        ch.publish(
+            exchange, 
+            routingKey, 
+            content, 
+            { contentType: "application/x-msgpack" },
             (err) => {
                 if (err !== null) {
                     reject(new Error("Message was NACKed by the broker"));
